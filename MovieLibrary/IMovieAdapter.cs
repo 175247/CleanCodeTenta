f2464b829c22b07fe411edde4d49f23ab6e47eba
175247 <yarr-tm@hotmail.com>
2021-01-21 12:57:34 +0100

    Removed unused using statements.

--- MovieLibrary/IMovieAdapter.cs
+++ MovieLibrary/IMovieAdapter.cs
@@ -1,8 +1,5 @@
 ﻿using MovieLibrary.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MovieLibrary
 {
--- MovieLibrary/Models/MovieDetailsDto.cs
+++ MovieLibrary/Models/MovieDetailsDto.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace MovieLibrary.Models
+﻿namespace MovieLibrary.Models
 {
     public class MovieDetailsDto
     {
